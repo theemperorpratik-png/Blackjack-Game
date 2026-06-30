8ddfb5bec84498ae177b24b8da68fd8640bba4ee
theemperorpratik-png <theemperorpratik@gmail.com>
2026-06-30 17:16:18 +0545

    Working with DOM to add interactivity and added UI

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 let firstCard = 11
 let secondCard = 10
+let message
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
-if ( sum <= 20) {
-    let message = " Do you want to draw another card?"
-} else if (sum === 21) {
-    let message = " wooohooo youve got blackjack!!!!!"
-    let hasBlackjack = true
-} else if (sum < 21) {
-    let message = " no youve lost the game"
-    let isAlive = false
+let messegeEl = document.getElementById("messege-el")
+let sumEl = document.querySelector("#sum-el")
+function startGame() {
+    if (sum <= 20) {
+        message = " Do you want to draw another card?"
+    } else if (sum === 21) {
+        message = " wooohooo youve got blackjack!!!!!"
+        hasBlackjack = true
+    } else if (sum > 21) {
+        message = " no youve lost the game"
+        isAlive = false
+    }
+    console.log(message)
+    console.log(isAlive)
+    console.log(hasBlackjack)
+    messegeEl.textContent = message
+    sumEl.textContent = "Sum:" + sum
 }
