755ab93b160f40e36a22d80500c7c416b3424a58
theemperorpratik-png <theemperorpratik@gmail.com>
2026-07-07 17:29:59 +0545

    fixed bugs
    the most twisted logic out there to get this to run

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let isAlive = false
 let messegeEl = document.getElementById("messege-el")
 let sumEl = document.querySelector("#sum-el")
 let cardEl = document.querySelector("#card-el")
-let dfc = getRandomCard() 
-let dsc = Math.floor(Math.random()*9 + 2)
+let dfc = getRandomCard()
+let dsc = Math.floor(Math.random() * 9 + 2)
 let win = false
 
 document.getElementById("but2").style.display = "none"
@@ -22,6 +22,12 @@ playerEl.textContent = player.name + ": $" + player.chips
 
 
 function renderGame() {
+    if (win) {
+        messegeEl.textContent = message
+        sumEl.textContent = "Sum: " + sum
+        cardEl.textContent = "Cards: " + cards.join(" ")
+        return
+    }
     if (sum <= 20 && win === false) {
         message = " Do you want to draw another card🤔?"
         document.getElementById("but0").style.display = "flex"
@@ -35,7 +41,7 @@ function renderGame() {
         playerEl.textContent = player.name + ": $" + "99999+"
         document.getElementById("but0").style.display = "none"
     } else if (sum > 21) {
-        
+
         message = " no youve lost the game😶😥"
         isAlive = false
         document.getElementById("but2").style.display = "none"
@@ -44,7 +50,7 @@ function renderGame() {
         document.getElementById("wan").style.backgroundColor = "red"
         playerEl.textContent = player.name + ": $" + 0
         document.getElementById("but0").style.display = "none"
-        
+
     }
     console.log(message)
     console.log(isAlive)
@@ -52,18 +58,20 @@ function renderGame() {
     messegeEl.textContent = message
     sumEl.textContent = "Sum:" + sum
     cardEl.textContent = "Cards: "
-    for (i = 0; i < cards.length; i++) {
+    for (let i = 0; i < cards.length; i++) {
         cardEl.textContent += cards[i] + " "
     }
 }
 function startGame() {
-    dfc = getRandomCard() 
-    dsc = Math.floor(Math.random()*9 + 2)
+    dfc = getRandomCard()
+    dsc = dsc = getRandomCard()
 
     document.getElementById("dealer").textContent = "Dealers Card: " + "?" + " " + dfc
     playerEl.textContent = player.name + ": $" + 67
     document.getElementById("wan").style.backgroundColor = "goldenrod"
-    document.getElementById("but1").textContent = "Start Game"
+    document.querySelector("#but1").innerHTML =
+        '<button id="wan" onclick="startGame()">Start Game</button>'
+    document.getElementById("wan").style.backgroundColor = "goldenrod"
     isAlive = true
     hasBlackjack = false
     let firstCard = getRandomCard()
@@ -73,9 +81,9 @@ function startGame() {
     document.getElementById("but2").style.display = "flex"
     document.getElementById("but1").style.display = "none"
     win = false
-    
+
     renderGame()
-    
+
 }
 function newCard() {
     if (isAlive === true && hasBlackjack === false) {
@@ -100,18 +108,20 @@ function getRandomCard() {
 function hit() {
     document.getElementById("dealer").textContent = "Dealers Card: " + dsc + " " + dfc
     let dsum = dsc + dfc
-    if(dsum > sum){
-       message = " no youve lost the game😶😥"
+    if (dsum > sum) {
+        message = " no youve lost the game😶😥"
         isAlive = false
         document.getElementById("but2").style.display = "none"
         document.getElementById("but1").style.display = "flex"
         document.querySelector("#but1").innerHTML = '<button id="wan" onclick="startGame()">Play Again</button></div>'
         document.getElementById("wan").style.backgroundColor = "red"
         playerEl.textContent = player.name + ": $" + 0
-        document.getElementById("but0").style.display = "none" 
+        document.getElementById("but0").style.display = "none"
+        win = true
+        isAlive = false
     }
-    if(dsum < sum) {
-       message = " wooo youve won the game!!🥳🥳"
+    if (dsum < sum) {
+        message = " wooo youve won the game!!🥳🥳"
         hasBlackjack = true
         document.getElementById("but2").style.display = "none"
         document.getElementById("but1").style.display = "flex"
@@ -119,8 +129,20 @@ function hit() {
         document.getElementById("wan").style.backgroundColor = "skyblue"
         playerEl.textContent = player.name + ": $" + "1000"
         document.getElementById("but0").style.display = "none"
-        win = true 
-        
+        win = true
+        isAlive = false
+
+    }
+    if (dsum === sum) {
+        message = " its a tieee!😶😶"
+        hasBlackjack = true
+        document.getElementById("but2").style.display = "none"
+        document.getElementById("but1").style.display = "flex"
+        document.querySelector("#but1").innerHTML = '<button id="wan" onclick="startGame()">Play Again</button></div>'
+        document.getElementById("wan").style.backgroundColor = "Gray"
+        document.getElementById("but0").style.display = "none"
+        win = true
+        isAlive = false
     }
     renderGame()
 }
